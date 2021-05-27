29bbe5758aa3ce0542be650057e21f834475d2a3
Alan <dittler.a@gmail.com>
2021-05-27 12:12:01 -0300

    added

---
+++ seek-and-destroy.js
@@ -0,0 +1,14 @@
+// Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note: You have to use the arguments object.
+
+function destroyer(arr) {
+  var valsToRemove = Array.from(arguments).slice(1);
+
+  return arr.filter((val) => {
+    return !valsToRemove.includes(val);
+  });
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
