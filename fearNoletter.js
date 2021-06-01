1292dc53c687e77c462d60bfa8077068b90ec7a7
Alan <dittler.a@gmail.com>
2021-06-01 17:56:50 -0300

    added

---
+++ fearNoletter.js
@@ -0,0 +1,20 @@
+// Missing letters
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+const fearNotLetter = (str) => {
+  let currCharCode = str.charCodeAt(0);
+
+  let missing = undefined;
+
+  str.split("").forEach((letter) => {
+    if (letter.charCodeAt(0) === currCharCode) {
+      currCharCode++;
+    } else {
+      missing = String.fromCharCode(currCharCode);
+    }
+  });
+
+  return missing;
+};
